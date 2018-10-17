e2ad0ef3b3a56758992106dba905664c91471eed
Steve Burian <steve.burian@utah.edu>
2018-10-17 06:57:04 -0600

    Added line of code to script.

---
+++ new_script.R
@@ -0,0 +1,11 @@
+#test script for GitHub
+myfunction <- function(x,y){
+  if(x>y){
+    print("x is greater than y")
+  }else{
+    print("y is greater than or equal to x")
+  }
+}
+
+#Let me test this
+z<-myfunction(x=10,y=5)
